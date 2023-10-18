364f65fcece5e4c5ae137fbf14fa6e2657c500bc
yogiset <the3verlastin9@gmail.com>
2023-10-19 04:15:43 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,23 @@ export default {
       href:'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css'
       },
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      { rel: "stylesheet", href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css' },  
+      {
+        href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
+        rel: 'stylesheet',
+        integrity:
+          'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
+        crossorigin: 'anonymous',
+      },
+      {rel:"stylesheet", href:"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css"},  
   
     ],
     scripts: [
-      {src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js',body: true},
+      {
+        src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
+        integrity:
+          'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4',
+        crossorigin: 'anonymous',
+      },
     ],
 
   },
